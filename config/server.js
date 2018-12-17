0ed645deb3ce23710d8a9600833794ac7c925008
Gerdon Mafra <gerdon_mafra@hotmail.com>
2018-12-17 09:11:02 -0300

    Alterado a porta do server

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 const restify = require('restify');
 const bodyParser = require('body-parser');
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = restify.createServer();
 
